ae521288d565dcb04f852b286bf28718ad4ec660
Kim Anh Nguyen <kimanhou@gmail.com>
2021-01-14 15:14:41 +0100

    Footer Maki logo

---
+++ src/view/components/Footer/FooterSocial.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './FooterSocial.scss';
+
+interface IFooterSocialProps {
+    classname : string;
+    link : string;
+}
+
+const FooterSocial : React.FC<IFooterSocialProps> = props => {
+
+    return(
+        <div className={`footer-social ${props.classname}`}>
+            <div className={`footer-social-background-hover`}></div>
+            <a href={props.link} target='_blank'/>
+        </div>
+    )
+}
+
+export default FooterSocial;
