b7bf7ec54f8be3318c3d01d31f6a8466ca774e6d
Kim Anh Nguyen <kimanhou@gmail.com>
2021-01-14 17:39:30 +0100

    Header

--- src/view/components/Home/Home.tsx
+++ src/view/components/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 import LatestPosts from '../LatestPosts/LatestPosts';
 import './Home.scss';
 
@@ -8,6 +9,7 @@ interface IHomeProps {}
 const Home : React.FC<IHomeProps> = props => {
     return (
         <div className={`home`}>
+            <Header />
             <LatestPosts />
             <Footer />
         </div>
---
+++ src/view/components/Navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Text from '../LocalisationContext/Text';
+import './Navigation.scss';
+import NavigationLink from './NavigationLink';
+
+interface INavigationProps {}
+
+const Navigation : React.FC<INavigationProps> = props => {
+    return (
+        <div className={`navigation`}>
+            <NavigationLink text={<Text english='Blog' french='Le blog' />} to='/posts' />
+            <NavigationLink text={<Text english='About' french='À propos' />} to='/about' />
+            <NavigationLink text={<Text english='Contact' french='Contact' />} to='/contact' />
+        </div>
+    );
+}
+
+export default Navigation;
