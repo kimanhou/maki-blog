fe89e878248c09a1709c01d3a16fc8af08556472
Kim Anh Nguyen <kimanhou@gmail.com>
2021-01-14 16:42:53 +0100

    Animation Section Header

--- src/view/components/LatestPosts/LatestPosts.tsx
+++ src/view/components/LatestPosts/LatestPosts.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ArticleModel from '../../../model/Article';
 import Article from '../Article/Article';
+import SectionHeader from '../SectionHeader/SectionHeader';
 import './LatestPosts.scss';
 import PostPreview from './PostPreview/PostPreview';
 
@@ -15,7 +16,7 @@ const LatestPosts : React.FC<ILatestPostsProps> = props => {
         <div className={`latest-posts`}>
             <div className={`main-wrapper`}>
                 <div className={`title`}>
-                    Latest on the blog
+                    <SectionHeader englishTitle='Latest' englishSubtitle='on the blog' frenchTitle='Les derniers articles' frenchSubtitle='sur le blog' />
                 </div>
                 <div className={`posts`}>
                     {posts.map(t => <PostPreview article={t}/>)}
