2af891e430d9a393c06be34596334c80b7c9fc80
Kim Anh Nguyen <kimanhou@gmail.com>
2021-01-15 14:33:41 +0100

    Fade in Footer

--- src/view/components/Footer/Footer.tsx
+++ src/view/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FadeIn from '../FadeIn/FadeIn';
 import MakiLogo from '../MakiLogo/MakiLogo';
 import MakiLogoWithNames from '../MakiLogo/MakiLogoWithNames';
 import './Footer.scss';
@@ -9,16 +10,18 @@ interface IFooterProps {}
 const Footer : React.FC<IFooterProps> = props => {
     return (
         <div className={`footer`}>
-            <div className={`main-wrapper`}>
-                <div className={`footer-left`}>
-                    Copyright © 2021 Maki
+            <FadeIn noDelay>
+                <div className={`main-wrapper`}>
+                    <div className={`footer-left`}>
+                        Copyright © 2021 Maki
+                    </div>
+                    <MakiLogoWithNames />
+                    <div className={`footer-right`}>
+                        <FooterSocial classname={`mail`} link={`mailto:mark.kimanh@gmail.com`}/>
+                        <FooterSocial classname={`instagram`} link={`https://www.instagram.com/thisismakiworld/`}/>
+                    </div>
                 </div>
-                <MakiLogoWithNames />
-                <div className={`footer-right`}>
-                    <FooterSocial classname={`mail`} link={`mailto:mark.kimanh@gmail.com`}/>
-                    <FooterSocial classname={`instagram`} link={`https://www.instagram.com/thisismakiworld/`}/>
-                </div>
-            </div>
+            </FadeIn>
         </div>
     );
 }
