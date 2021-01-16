57222ab43fa849ffea0ae4d1094b0c563488bdcb
Kim Anh Nguyen <kimanhou@gmail.com>
2021-01-16 17:32:39 +0100

    Back to Category ALL

--- src/view/components/Post/PostTemplate/TemplateComponents/ArrowBack.tsx
+++ src/view/components/Post/PostTemplate/TemplateComponents/ArrowBack.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Category from '../../../../../model/Category';
-import { myScrollTo } from '../../../../../Util';
 import FadeIn from '../../../FadeIn/FadeIn';
 import './ArrowBack.scss';
 
 interface IArrowBackProps {
-    // categories : Category[];
 }
 
 const ArrowBack : React.FC<IArrowBackProps> = props => {
     return(
-        <Link className={`arrow-back`} to={`/posts?${Category.toQueryParam([ Category.ALL ])}`}>
+        <Link className={`arrow-back`} to={`/posts?${Category.toQueryParam( [ Category.ALL ] )}`}>
             <FadeIn noDelay>
                 <span className={`arrow-back-left`}>&#60;</span>
                 <span className={`arrow-back-text`}>Back</span>
