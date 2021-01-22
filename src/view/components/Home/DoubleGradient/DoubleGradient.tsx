8f08a3aca8b72cf102e972c1cb3efe8f9b5babc4
Kim Anh Nguyen <kimanhou@gmail.com>
2021-01-22 11:50:26 +0100

    Home page

---
+++ src/view/components/Home/DoubleGradient/DoubleGradient.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DoubleGradient.scss';
+
+const DoubleGradient : React.FC = props => {
+    return (
+        <div className={`double-gradient`}/>
+    );
+}
+
+export default DoubleGradient;
--- src/view/components/Home/Intro/Intro.tsx
+++ src/view/components/Home/Intro/Intro.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Text from '../../LocalisationContext/Text';
 import './Intro.scss';
 
 const Intro : React.FC = props => {
+    const [active, setActive] = useState(false);
+    const activeClassName = active ? 'active' : '';
+
+    useEffect(() => {
+        const onScroll = () => {
+            setActive(true);
+            window.removeEventListener('scroll', onScroll);
+        };
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
+
     return (
-        <div className={`intro`}>
-            <div className={`intro-picture`} ></div>
-            <div className={`intro-text`}>
-                <p><Text english='We talk about web design, coding and stuff.' french="On parle de conception web, de code et autre." /></p>
-                <p><Text english='We believe in minimalist designs, clean code and constant improvement.' french="On aime les concepts minimalistes et le code propre." /></p>
-            </div>
+        <div className={`intro ${activeClassName}`}>
+            <p><Text english='We talk about web design, coding and stuff.' french="On parle de conception web, de code et autre." /></p>
+            <p><Text english='We believe in minimalist designs, clean code and constant improvement.' french="On aime les concepts minimalistes et le code propre." /></p>
         </div>
     );
 }
