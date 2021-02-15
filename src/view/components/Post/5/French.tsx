de9d5bbeb0675a7efeeaf37588ebb7b1b167c943
Kim Anh Nguyen <kimanhou@gmail.com>
2021-02-15 18:29:58 +0100

    WIP example 2

--- src/view/components/Post/5/French.tsx
+++ src/view/components/Post/5/French.tsx
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { IPostContentProps } from '../../../../model/Post';
+import { myScrollToWithOffset } from '../../../../Util';
+import { isMobile } from '../../../hooks/UseMediaQuery';
 import Text from '../../LocalisationContext/Text';
 import PostTemplate, { getImageSrc } from '../PostTemplate/PostTemplate';
 import Button from '../PostTemplate/TemplateComponents/Button';
 import Cross from '../PostTemplate/TemplateComponents/Cross';
-import { example1RenderColumns, getRandomHeight, useDemoAlignItems, useDemoExample1ControlColumns, useDemoFlex, useDemoFlexDirection } from './Post5Hooks';
+import { example1RenderColumns, example2RenderColumns, getRandomHeight, getRandomNumberOfSentences, useDemoAlignItems, useDemoExample1ControlColumns, useDemoFlex, useDemoFlexDirection } from './Post5Hooks';
 import './PostPage.scss';
 
 const French : React.FC<IPostContentProps> = props => {
     const postId = 5;
+    const mobile = isMobile();
     const tldr = 
     <>
         <Cross/><span></span>
@@ -30,8 +33,33 @@ const French : React.FC<IPostContentProps> = props => {
     const [threeFlex1, onClickThreeFlex1] = useDemoFlex();
     const demoChildFlex1ActiveClassname3 = threeFlex1 == '1' ? 'active' : '';
 
-    const [example1ColumnNumber, example1AddColumn, example1RemoveColumn] = useDemoExample1ControlColumns();
-    const [heights] = useState(() => Array.from(Array(10).keys()).map(getRandomHeight));
+    const columnNumberMax = mobile ? 5 : 10;
+    const gutter = mobile ? 10 : 20;
+    const example1Height = mobile ? 200 : 400;
+    const [example1ColumnNumber, example1AddColumn, example1RemoveColumn] = useDemoExample1ControlColumns(columnNumberMax);
+    const computeHeights = (mobile : boolean) => {
+        return Array.from(Array(10).keys()).map(t => getRandomHeight(mobile));
+    }
+    const [heights, setHeights] = useState(() => computeHeights(mobile));
+    useEffect(() => {
+        setHeights(computeHeights(mobile));
+    }, [mobile]);
+
+    const [example2ColumnNumber, example2AddColumn, example2RemoveColumn] = useDemoExample1ControlColumns(columnNumberMax);
+    const computeSentences = (mobile : boolean) => {
+        return Array.from(Array(10).keys()).map(t => getRandomNumberOfSentences(mobile));
+    }
+    const [sentences, setSentences] = useState(() => computeSentences(mobile));
+    useEffect(() => {
+        setSentences(computeSentences(mobile));
+    }, [mobile]);
+
+    const example2flex = mobile ? 80 : 150;
+
+
+    const goToDemo = () => {
+        myScrollToWithOffset('fun', -190);
+    }
     
     return (
         <PostTemplate postId={postId}
@@ -104,21 +132,26 @@ const French : React.FC<IPostContentProps> = props => {
 
             <p>
                 Si on veut organiser ce <span className={`code-element`}>div</span> parent en lignes, c'est-à-dire le diviser en plusieurs lignes les unes au-dessus des autres, il faut lui attribuer une direction en colonne.
+                Les propriétés <i>align-items</i> et <i>justify-content</i> sont alors inversées car l'axe principal et l'axe secondaire est inversé : <i>align-items</i> contrôle alors l'alignement horizontal et <i>justify-content</i> l'alignement vertical.
+            </p>
+            <p>
+                Pour plus de clarté, vous pouvez jouer avec <span onClick={goToDemo} className={`cursor-pointer`}>le module de démonstration</span>.
             </p>
             <div className={`code-sample`}>
                 <span className={`color-vs-light-blue`}>display</span>: <span className={`color-vs-orange`}>flex</span>;<br></br>
-                <span className={`color-vs-light-blue`}>flex-direction</span>: <span className={`color-vs-orange`}>row</span>;
+                <span className={`color-vs-light-blue`}>flex-direction</span>: <span className={`color-vs-orange`}>column</span>;
             </div>
 
             <img src={getImageSrc(postId, 'temp.png')}/>
 
             <div className={`section-separator`}></div>
 
-            <Cross/><span className={`step-title`}>2. Les exemples courants</span>
-            <p>
+            <Cross/><span className={`step-title`}>2. Les exemples courants</span><br></br>
+            <br></br>
+            <Cross/><span>
                 Diviser un <span className={`code-element`}>div</span> en colonnes de largeur égale et séparées entre elles par une marge, le tout centrer horizontalement.
-            </p>
-            <p>Cliquer sur les boutons ci-dessous pour ajouter ou supprimer des colonnes (nombre maximal de colonnes : 10).</p>
+            </span>
+            <p>Cliquer sur les boutons ci-dessous pour ajouter ou supprimer des colonnes (nombre maximal de colonnes : {columnNumberMax}).</p>
             
             <div className={`example-1-consignes`}>
                 <div className={`example-1-buttons-circle`}>
@@ -131,7 +164,7 @@ const French : React.FC<IPostContentProps> = props => {
                         <p className={`example-1-button-column-number`}>
                             {example1ColumnNumber}
                         </p>
-                        <Button onClick={example1AddColumn} classname={`demo-button`} isUnselected={example1ColumnNumber > 9}>
+                        <Button onClick={example1AddColumn} classname={`demo-button`} isUnselected={example1ColumnNumber > columnNumberMax - 1}>
                                 +
                         </Button>
                     </div>
@@ -149,8 +182,8 @@ const French : React.FC<IPostContentProps> = props => {
             
             <p>
                 Le <span className={`code-element`}>div</span> parent est un container flex en ligne. 
-                Chaque enfant a un <span className={`code-element`}>flex: 1;</span> et une marge de 10px de chaque côté donc il y a un écart de 20px entre chaque enfant.
-                La hauteur et la couleur de fond des enfants sont contrôlés programmatiquement : la hauteur est aléatoire et comprise entre 80 et 400px, tandis que la couleur de fond varie d'un bleu très clair au <i>darkslategray</i> pour créer un joli camaïeu.
+                Chaque enfant a un <span className={`code-element`}>flex: 1;</span> et une marge de {gutter / 2}px de chaque côté donc il y a un écart de {gutter}px entre chaque enfant.
+                La hauteur et la couleur de fond des enfants sont contrôlés programmatiquement : la hauteur est aléatoire et comprise entre 80 et {example1Height}px, tandis que la couleur de fond varie d'un bleu très clair au <i>darkslategray</i> pour créer un joli camaïeu.
             </p>
 
             <span className={`code-title`}>CSS</span>
@@ -161,15 +194,74 @@ const French : React.FC<IPostContentProps> = props => {
             <span className={`nbsp`}/><span className={`color-vs-light-blue`}>border</span>: <span className={`color-vs-orange`}>solid</span> <span className={`color-vs-light-green`}>1px</span> <span className={`color-vs-orange`}>black</span>;<br></br>
             <span className={`nbsp`}/><span className={`color-vs-light-blue`}>border-radius</span>: <span className={`color-vs-light-green`}>10px</span>;<br></br>
             <span className={`nbsp`}/><span className={`color-vs-light-blue`}>margin</span>: <span className={`color-vs-light-green`}>0</span> <span className={`color-vs-orange`}>auto</span>;<br></br>
-            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>width</span>: <span className={`color-vs-yellow`}>calc</span>(<span className={`color-vs-light-green`}>100%</span> - <span className={`color-vs-light-green`}>2px</span>);<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>width</span>: <span className={`color-vs-light-green`}>80%</span>;<br></br>
             <span className={`nbsp`}/><span className={`color-vs-light-blue`}>height</span>: <span className={`color-vs-light-green`}>400px</span>;<br></br>
-            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>padding</span>: <span className={`color-vs-light-green`}>0 -10px</span>;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>padding</span>: <span className={`color-vs-light-green`}>0 -{gutter / 2}px</span>;<br></br>
             <span className={`nbsp`}/>&gt;<span className={`color-vs-light-orange`}>.div-enfant</span> &#123;<br></br>
             <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>flex</span>: <span className={`color-vs-light-green`}>1</span>;<br></br>
-            <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>margin</span>: <span className={`color-vs-light-green`}>0 10px</span>;<br></br>
+            <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>margin</span>: <span className={`color-vs-light-green`}>0 {gutter / 2}px</span>;<br></br>
             <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>border</span>: <span className={`color-vs-orange`}>solid</span> <span className={`color-vs-light-green`}>1px</span> <span className={`color-vs-orange`}>darkslategray</span>;<br></br>
             <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>border-radius</span>: <span className={`color-vs-light-green`}>10px</span>;<br></br>
             <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>font-size</span>: <span className={`color-vs-light-green`}>3rem</span>;<br></br>
+            <span className={`nbsp`}/>&#125;<br></br>
+            &#125;
+            </div>
+
+            <div className={`section-separator`}></div>
+
+            <Cross/><span>
+                Ajouter des enfants dans un <span className={`code-element`}>div</span> parent sans déborder avec la propriété <i>flex-wrap</i>. Si les enfants ne rentrent pas sur une ligne, ils vont automatiquement se placer dans la ligne d'en dessous.
+            </span>
+            <div className={`example-1-consignes`}>
+                <div className={`example-1-buttons-circle`}>
+                    <div className={`flex-row example-1-buttons`}>
+                        <p className={`example-1-button-text`}>Nombre de colonnes : </p>
+                        <Button onClick={example2RemoveColumn} classname={`demo-button`} isUnselected={example2ColumnNumber < 1}>
+                                -
+                        </Button>
+                        <p className={`example-1-button-column-number`}>
+                            {example2ColumnNumber}
+                        </p>
+                        <Button onClick={example2AddColumn} classname={`demo-button`} isUnselected={example2ColumnNumber > columnNumberMax - 1}>
+                                +
+                        </Button>
+                    </div>
+                </div>
+            </div>
+            <div className={`flex-row demo-button-line`}>
+                <Button onClick={onClickFlexDirection} classname={`demo-button`} isUnselected={flexDirection == 'row'}>
+                    flex: {example2flex}px;
+                </Button>
+                <Button onClick={onClickFlexDirection} classname={`demo-button`} isUnselected={flexDirection == 'column'}>
+                    width: {example2flex}px;
+                </Button>
+            </div>
+            <div className={`example-2`}>
+                {example2RenderColumns(example2ColumnNumber, sentences)}
+            </div>
+            <p>
+                Les enfants prennent toujours toute la place disponible au sein d'une ligne.
+            </p>
+            <span className={`code-title`}>CSS</span>
+            <div className={`code-sample`}>
+            <span className={`color-vs-light-orange`}>.div-parent</span> &#123;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>display</span>: <span className={`color-vs-orange`}>flex</span>;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>flex-direction</span>: <span className={`color-vs-orange`}>row</span>;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>flex-wrap</span>: <span className={`color-vs-orange`}>wrap</span>;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>justify-content</span>: <span className={`color-vs-orange`}>center</span>;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>border</span>: <span className={`color-vs-orange`}>solid</span> <span className={`color-vs-light-green`}>1px</span> <span className={`color-vs-orange`}>black</span>;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>border-radius</span>: <span className={`color-vs-light-green`}>10px</span>;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>margin</span>: <span className={`color-vs-light-green`}>0</span> <span className={`color-vs-orange`}>auto</span>;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>width</span>: <span className={`color-vs-light-green`}>80%</span>;<br></br>
+            <span className={`nbsp`}/><span className={`color-vs-light-blue`}>padding</span>: <span className={`color-vs-light-green`}>0 -{gutter / 2}px</span>;<br></br>
+            <span className={`nbsp`}/>&gt;<span className={`color-vs-light-orange`}>.div-enfant</span> &#123;<br></br>
+            <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>flex</span>: <span className={`color-vs-light-green`}>{example2flex}px</span>;<br></br>
+            <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>margin</span>: <span className={`color-vs-light-green`}>0 {gutter / 2}px {gutter / 2}px</span>;<br></br>
+            <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>border</span>: <span className={`color-vs-orange`}>solid</span> <span className={`color-vs-light-green`}>1px</span> <span className={`color-vs-orange`}>darkslategray</span>;<br></br>
+            <span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>border-radius</span>: <span className={`color-vs-light-green`}>10px</span>;<br></br>
+            <span className={`nbsp`}/><span className={`nbsp`}/>&gt;<span className={`color-vs-light-orange`}>.number</span> &#123;<br></br>
+            <span className={`nbsp`}/><span className={`nbsp`}/><span className={`nbsp`}/><span className={`color-vs-light-blue`}>font-size</span>: <span className={`color-vs-light-green`}>3rem</span>;<br></br>
+            <span className={`nbsp`}/><span className={`nbsp`}/>&#125;<br></br>
             <span className={`nbsp`}/>&#125;<br></br>
             &#125;
             </div>
@@ -181,7 +273,7 @@ const French : React.FC<IPostContentProps> = props => {
                 Cliquer sur les boutons pour modifier les propiétés du <span className={`code-element`}>div</span> parent et cliquer sur les enfants pour contrôler leur taille grâce à la propritété <span className={`code-element`}>flex: 1;</span>.
             </p>
 
-            <div className={`flex-row demo-button-line-1`}>
+            <div className={`flex-row demo-button-line`}>
                 <Button onClick={onClickFlexDirection} classname={`demo-button`} isUnselected={flexDirection == 'row'}>
                     flex-direction: column;
                 </Button>
@@ -189,7 +281,7 @@ const French : React.FC<IPostContentProps> = props => {
                     flex-direction: row;
                 </Button>
             </div>
-            <div className={`flex-row demo-button-line-2`}>
+            <div className={`flex-row demo-button-line`}>
                 <Button onClick={onClickDemoAlignItemsStart} classname={`demo-button`} isUnselected={alignItems != 'flex-start'}>
                     align-items: flex-start;
                 </Button>
@@ -200,7 +292,7 @@ const French : React.FC<IPostContentProps> = props => {
                     align-items: flex-end;
                 </Button>
             </div>
-            <div className={`flex-row demo-button-line-3`}>
+            <div className={`flex-row demo-button-line`}>
                 <Button onClick={onClickDemoJustifyContentStart} classname={`demo-button`} isUnselected={justifyContent != 'flex-start'}>
                     justify-content: flex-start;
                 </Button>
--- src/view/components/Post/5/Post5Hooks.tsx
+++ src/view/components/Post/5/Post5Hooks.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useState } from 'react';
 
-export const useDemoExample1ControlColumns = () : [ number, () => void, () => void ] => {
+export const useDemoExample1ControlColumns = (columnNumberMax : number) : [ number, () => void, () => void ] => {
     const [columnNumber, setColumnNumber] = useState(3);
     const addColumn = () => {
-        if (columnNumber < 10){
+        if (columnNumber < columnNumberMax){
             setColumnNumber(columnNumber + 1);
         }
     }
@@ -38,17 +38,17 @@ const getColor = (rgb : string) => {
         return 'var(--color-white)';
     }
 }
-export const getRandomHeight = () => {
+export const getRandomHeight = (isMobile : boolean) => {
     const min = Math.ceil(80);
-    const max = Math.floor(400);
+    const max = isMobile ? Math.floor(200) : Math.floor(400);
     return Math.floor(Math.random() * (max - min) + min);
 }
-const example1RenderOneColumn = (index : number, columnNumber : number, heights : number[]) => {
+const example1RenderOneColumn = (index : number, columnNumber : number, heights : number[], classname : string) => {
     const backgroundColor = getBackgroundColor(index, columnNumber - 1);
     const color = getColor(backgroundColor);
     const height = heights[index];
     return (
-        <div className={`example-1-column`} style={{ backgroundColor: backgroundColor, color: color, height: height }}>
+        <div className={classname} style={{ backgroundColor: backgroundColor, color: color, height: height }}>
             {index + 1}
         </div>
     );
@@ -57,7 +57,41 @@ const example1RenderOneColumn = (index : number, columnNumber : number, heights 
 export const example1RenderColumns = (columnNumber : number, heights : number[]) => {
     if (columnNumber > 0) {
         var foo = Array.from(Array(columnNumber).keys());
-        return foo.map(t => example1RenderOneColumn(t, columnNumber, heights));
+        return foo.map(t => example1RenderOneColumn(t, columnNumber, heights, `example-1-column`));
+    }
+}
+
+
+const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a sapien varius justo interdum malesuada. Nulla blandit nunc lacus. Phasellus mi sapien, vestibulum sit amet turpis vitae, malesuada tincidunt quam. Phasellus at scelerisque sapien. Integer id augue a enim pharetra bibendum sed non ipsum. Vivamus justo nisi, tincidunt sit amet placerat sit amet, dignissim id urna. Curabitur eget diam nec nisi luctus volutpat. Morbi aliquam sollicitudin sem, vel tristique ex placerat nec. Sed eu magna sit amet dolor interdum hendrerit ut ut quam. Aliquam neque libero, mattis eu ipsum tincidunt, maximus fringilla tortor. Nam semper feugiat dictum. Nullam euismod felis id feugiat faucibus.';
+const lorempIpsumSplit = loremIpsum.split('. ');
+const loremIpsumNumberOfSentencesMax = lorempIpsumSplit.length;
+
+export const getRandomNumberOfSentences = (isMobile : boolean) => {
+    const min = Math.ceil(1);
+    const max = isMobile ? Math.floor(5) : Math.floor(loremIpsumNumberOfSentencesMax);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const getText = (numberOfSentences : number) => {
+    return lorempIpsumSplit.slice(0, numberOfSentences).join('. ');
+}
+
+const example2RenderOneColumn = (index : number, columnNumber : number, classname : string, sentences : number[]) => {
+    const backgroundColor = getBackgroundColor(index, columnNumber - 1);
+    const color = getColor(backgroundColor);
+    const text = getText(sentences[index]);
+    return (
+        <div className={classname} style={{ backgroundColor: backgroundColor, color: color}}>
+            <span className={`number`}>{index + 1}</span><br></br>
+            {text}
+        </div>
+    );
+}
+
+export const example2RenderColumns = (columnNumber : number, sentences : number[]) => {
+    if (columnNumber > 0) {
+        var foo = Array.from(Array(columnNumber).keys());
+        return foo.map(t => example2RenderOneColumn(t, columnNumber, `example-2-column`, sentences));
     }
 }
 
