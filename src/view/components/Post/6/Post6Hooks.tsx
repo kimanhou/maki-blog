b90a72b5e71c15bfc0ef8464b0681697232299de
Kim Anh Nguyen <kimanhou@gmail.com>
2021-02-18 16:53:44 +0100

    Pokeball 16

--- src/view/components/Post/6/Post6Hooks.tsx
+++ src/view/components/Post/6/Post6Hooks.tsx
@@ -35,42 +35,92 @@ const getBackgroundColor4x4 = (x : number, y : number) => {
 }
 
 const getBackgroundColor8x8 = (x : number, y : number) : string => {
-    if (x == 0 && (y == 1 || y == 2 || y == 7 )) {
-        return 'var(--color-off-black)';
-    }
-    if (x == 3 && (y == 4 || y == 7 )) {
-        return 'black';
-    }
-    if (x == 4 && y == 3) {
+    const redCoordinates = [ { x : 4, y : 3 } ];
+    const blackCoordinates = [ { x : 3, y : 4 }, { x : 3, y : 7 } ];
+    const offBlackCoordinates = [ { x : 0, y : 1 }, { x : 0, y : 2 }, { x : 0, y : 7 } ];
+    const brownCoordinates = [ { x : 5, y : 1 }, { x : 6, y : 1 }, { x : 7, y : 4 }, { x : 7, y : 5 } ];
+    const yellowCoordinates = [
+        { x : 1, y : 2 },
+        { x : 2, y : 3 }, { x : 2, y : 4 }, { x : 2, y : 5 }, { x : 2, y : 6 },
+        { x : 3, y : 3 }, { x : 3, y : 5 }, { x : 3, y : 6 },
+        { x : 4, y : 4 }, { x : 4, y : 5 }, { x : 4, y : 6 },
+        { x : 5, y : 3 },
+        { x : 6, y : 2 }, { x : 6, y : 4 }, { x : 6, y : 6 },
+        { x : 7, y : 2 }
+    ];
+    const darkYellowCoordinates = [
+        { x : 1, y : 3 }, { x : 1, y : 7 },
+        { x : 2, y : 7 },
+        { x : 3, y : 0 }, { x : 3, y : 1 },
+        { x : 4, y : 0 }, { x : 4, y : 1 }, { x : 4, y : 7 },
+        { x : 5, y : 4 }, { x : 5, y : 5 }, { x : 5, y : 6 },
+        { x : 6, y : 3 }, { x : 6, y : 5 },
+        { x : 7, y : 3 }, { x : 7, y : 6 }
+    ];
+
+    if (isInList(x, y, redCoordinates)) {
         return 'red';
     }
-    if ((x == 1 && y == 2) 
-        || (x == 2 && (y == 3 || y == 4 || y == 5 || y == 6))
-        || (x == 3 && (y == 3 || y == 5 || y == 6))
-        || (x == 4 && (y == 4 || y == 5 || y == 6))
-        || (x == 5 && y == 3)
-        || (x == 6 && (y == 2 || y == 4 || y == 6))
-        || (x == 7 && y == 2)) {
+    if (isInList(x, y, blackCoordinates)) {
+        return 'black';
+    }
+    if (isInList(x, y, offBlackCoordinates)) {
+        return 'var(--color-off-black)';
+    }
+    if (isInList(x, y, brownCoordinates)) {
+        return 'rgb(197,109,26)';
+    }
+    if (isInList(x, y, yellowCoordinates)) {
         return 'var(--color-yellow)';
     }
-    if ((x == 1 && (y == 3 || y == 7)) 
-        || (x == 2 && y == 7)
-        || (x == 3 && (y == 0 || y == 1))
-        || (x == 4 && (y == 0 || y == 1 || y == 7))
-        || (x == 5 && (y == 4 || y == 5 || y == 6))
-        || (x == 6 && (y == 3 || y == 5))
-        || (x == 7 && (y == 3 || y == 6))) {
+    if (isInList(x, y, darkYellowCoordinates)) {
         return 'rgb(253,152,39)';
-    }
-    if ((x == 5 && y ==1)
-        || (x == 6 && y ==1)
-        || (x == 7 && (y == 4 || y == 5))) {
-        return 'rgb(197,109,26)';
     }
     return '';
 }
 
-const getBackgroundColor16x16 = (x : number, y : number) => {
+const getBackgroundColor16x16Pokeball = (x : number, y : number) => {
+    const blackCoordinates = [ 
+        {x : 1, y: 6}, {x : 1, y: 7}, {x : 1, y: 8}, {x : 1, y: 9},
+        {x : 2, y: 4}, {x : 2, y: 5}, {x : 2, y: 10}, {x : 2, y: 11},
+        {x : 3, y: 3}, {x : 3, y: 12},
+        {x : 4, y: 2}, {x : 4, y: 12}, {x : 4, y: 13},
+        {x : 5, y: 2}, {x : 5, y: 13},
+        {x : 6, y: 1}, {x : 6, y: 14},
+        {x : 7, y: 1}, {x : 7, y: 2}, {x : 7, y: 3}, {x : 7, y: 8}, {x : 7, y: 9}, {x : 7, y: 10}, {x : 7, y: 14},
+        {x : 8, y: 1}, {x : 8, y: 2}, {x : 8, y: 3}, {x : 8, y: 4}, {x : 8, y: 7}, {x : 8, y: 11}, {x : 8, y: 14},
+        {x : 9, y: 1}, {x : 9, y: 5}, {x : 9, y: 6}, {x : 9, y: 7}, {x : 9, y: 11}, {x : 9, y: 12}, {x : 9, y: 13}, {x : 9, y: 14},
+        {x : 10, y: 2}, {x : 10, y: 6}, {x : 10, y: 7}, {x : 10, y: 11}, {x : 10, y: 13},
+        {x : 11, y: 2}, {x : 11, y: 8}, {x : 11, y: 9}, {x : 11, y: 10}, {x : 11, y: 13},
+        {x : 12, y: 3}, {x : 12, y: 12},
+        {x : 13, y: 4}, {x : 13, y: 5}, {x : 13, y: 10}, {x : 13, y: 11},
+        {x : 14, y: 6}, {x : 14, y: 7}, {x : 14, y: 8}, {x : 14, y: 9},
+    ];
+
+    const whiteCoordinates = [ { x : 4, y : 5 }, { x : 5, y : 4 } ];
+    const greyCoordinates = [ { x : 9, y : 2 }, { x : 11, y : 3 }, { x : 12, y : 4 }, { x : 13, y : 6 }, { x : 13, y : 7 }];
+    const redCoordinates = [ 
+        { x : 2, y : 6 }, { x : 2, y : 7 }, { x : 2, y : 8 }, { x : 2, y : 9 },
+        { x : 3, y : 4 }, { x : 3, y : 5 }, { x : 3, y : 6 }, { x : 3, y : 7 }, { x : 3, y : 8 }, { x : 3, y : 9 }, { x : 3, y : 10 }, { x : 3, y : 11 },
+        { x : 4, y : 3 }, { x : 4, y : 4 }, { x : 4, y : 6 }, { x : 4, y : 7 }, { x : 4, y : 8 }, { x : 4, y : 9 }, { x : 4, y : 10 }, { x : 4, y : 11 },
+        { x : 5, y : 3 }, { x : 5, y : 5 }, { x : 5, y : 6 }, { x : 5, y : 7 }, { x : 5, y : 8 }, { x : 5, y : 9 }, { x : 5, y : 10 }, { x : 5, y : 11 }, { x : 5, y : 12 },
+        { x : 6, y : 2 }, { x : 6, y : 3 }, { x : 6, y : 4 }, { x : 6, y : 5 }, { x : 6, y : 6 }, { x : 6, y : 7 }, { x : 6, y : 8 }, { x : 6, y : 9 }, { x : 6, y : 10 }, { x : 6, y : 11 }, { x : 6, y : 12 }, { x : 6, y : 13 }, { x : 6, y : 14 },
+        { x : 7, y : 4 }, { x : 7, y : 5 }, { x : 7, y : 6 }, { x : 7, y : 7 }, { x : 7, y : 11 }, { x : 7, y : 12 }, { x : 7, y : 13 },
+        { x : 8, y : 5 }, { x : 8, y : 6 }, { x : 8, y : 12 }, { x : 8, y : 13 }
+    ];
+
+    if (isInList(x, y, blackCoordinates)) {
+        return 'black';
+    }
+    if (isInList(x, y, whiteCoordinates)) {
+        return 'white';
+    }
+    if (isInList(x, y, greyCoordinates)) {
+        return 'rgb(158,158,158)';
+    }
+    if (isInList(x, y, redCoordinates)) {
+        return 'rgb(229,57,53)';
+    }
     return '';
 }
 
@@ -85,7 +135,7 @@ const getBackgroundColorFunction = (size : number) : ((x : number, y : number) =
         case 8:
             return getBackgroundColor8x8;
         case 16:
-            return getBackgroundColor16x16;
+            return getBackgroundColor16x16Pokeball;
         case 32:
             return getBackgroundColor32x32;
         default:
@@ -93,3 +143,9 @@ const getBackgroundColorFunction = (size : number) : ((x : number, y : number) =
     }
 }
 
+const isInList = (x : number, y : number, myArray : {x : number, y : number}[]) => {
+    if (myArray.find(t => t.x == x && t.y == y)) {
+        return true;
+    }
+    return false;
+}
