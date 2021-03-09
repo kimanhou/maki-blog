9ef45c67641420e39cc660d21838696ba4dd7402
Kim Anh Nguyen <kimanhou@gmail.com>
2021-03-09 11:47:13 +0100

    Install Gastby

---
+++ .cache/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/kimanhnguyen/Documents/Sites/thisismakiworld/Code/.cache/caches/gatsby-plugin-offline/app-shell.js"))
+}
+
